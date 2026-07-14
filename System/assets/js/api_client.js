83e322246388472795cf661cfe846178c75bf87c
Thejani Perera <thejani.sandamini@gmail.com>
2026-07-14 10:30:57 +0530

    update api_client.js
    add restock API

--- System/assets/js/api_client.js
+++ System/assets/js/api_client.js
@@ -89,5 +89,12 @@ const OrdersAPI = {
     updateStatus: (order_id, status) => API.call('orders', 'updateStatus', 'POST', { order_id, status }),
 };
 
-// TODO : Restock APi
+//  Restock APi
+const RestockAPI = {
+    list: () => API.call('restock', 'list'),
+    create: (data) => API.call('restock', 'create', 'POST', data),
+    update: (data) => API.call('restock', 'update', 'POST', data),
+    delete: (id) => API.call('restock', 'delete', 'POST', { id }),
+    suppliers: () => API.call('restock', 'suppliers'),
+};
 // TODO : Leave API
