efefdccfe790218c67153784b98ef2992046b690
thidassandaruwan <170103578+thidassandaruwan@users.noreply.github.com>
2026-07-17 14:00:14 +0530

    Update database

--- System/database/schema.sql
+++ System/database/schema.sql
@@ -85,7 +85,7 @@ CREATE TABLE IF NOT EXISTS orders (
     customer_id INT, -- Can be NULL for walk-in customers
     customer_name VARCHAR(100), -- For walk-in or quick entry
     order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status ENUM('Pending', 'Preparing', 'Ready for Pickup', 'Out for Delivery', 'Delivered', 'Completed', 'Cancelled', 'Rejected') DEFAULT 'Pending',
+    status ENUM('Pending', 'Preparing', 'Ready for Pickup', 'Out for Delivery', 'Delivered', 'Completed', 'Cancelled', 'Approved' 'Rejected') DEFAULT 'Pending',
     total_amount DECIMAL(10, 2) NOT NULL,
     order_type ENUM('Online', 'InStore', 'Custom') NOT NULL,
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
@@ -103,9 +103,12 @@ CREATE TABLE IF NOT EXISTS order_items (
 
 CREATE TABLE IF NOT EXISTS online_orders (
     order_id INT PRIMARY KEY,
-    delivery_address TEXT NOT NULL,
-    tracking_number VARCHAR(50),
-    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE
+    customer_id INT,
+    design_details TEXT,
+    description TEXT,
+    pickup_date DATE,
+    status ENUM('PendingApproval', 'Approved', 'Rejected') DEFAULT 'PendingApproval',
+    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE,
 );
 
 CREATE TABLE IF NOT EXISTS custom_cake_orders (
